70e573a40d84b3bfb841a60da14a3e759bab2d18
Damian Baran <damian1baran@gmail.com>
2016-06-02 21:23:26 +0200

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Bassy on 2. 6. 2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Tim!");
+
+        int myFirstNumber = (10 + 2) * 2;
+        System.out.println(myFirstNumber);
+
+        System.out.println("myFirstNumber");
+
+        int mySecondNumber = 3;
+        int myThirdNumber = 7;
+        int myTotal;
+
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+    }
+}
